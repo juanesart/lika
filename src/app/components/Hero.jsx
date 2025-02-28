3d0940309ab81bd666ecd9c1b236da6454fbd204
juanesart <juanesartq@gmail.com>
2025-02-28 16:33:52 -0500

    first commit

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,43 @@
+import Music from "./Music";
+
+function Hero() {
+    return (
+        <div className="hero" id="hero">
+            <div className="">
+                <div className="hero-content text-center">
+                    <div className="max-w-[70%]">
+                        <div className="mx-5 py-10 bg-white/50">
+                            <h1 className=" text-5xl font-bold">inmarcesible</h1>
+                            <p className="my-5 font-medium text-2xl">Bueno, no sé realmente como empezar esto, ni que decir o no en este momento, pero aquí va un intento.</p>
+
+                            <p className="my-5 font-medium text-2xl">Voy a empezar bien cliché, justo como a ti te gusta, diciendo que eres una gran persona que a ratos se pasa de pendeja, que muchas veces se echa responsabilidades encima que no le competen, que se preocupa por su entorno y quienes le rodean a veces en exceso, que no ha tenido ciertamente las mejores experiencias en muchos aspectos de la vida y que la soledad puede llegar a volverse tanto un consuelo como un abismo terrorífico.</p>
+                            <p className="my-5 font-medium text-2xl">Tal como dice la canción.</p>
+                            <Music track1="/EternaSoledad.mp3" />
+
+                            <p className="my-5 font-medium text-2xl">Y que aún así, has podido seguir adelante, así que aunque tu cabeza te diga que no o te intentes sabotear, yo estoy seguro de que saldrás de esto y aunque para ti pueda sonar como palabrería sin sentido, yo quiero creer en que realmente la vida luego de reírse un poco (demasiado) de uno le sonríe de alguna manera y que tú no serás la excepción.</p>
+                            <p className="my-5 font-medium text-2xl">Eso en primera instancia.</p>
+
+                            <p className="my-5 font-medium text-2xl">Luego de lo que pueda o no pasar entre nosotros, debo decir que mi punto de vista no ha cambiado mucho jaja, si me dijeras que quisieras ver hasta el final lo que nos depara sin importar lo que haya que enfrentar seguramente te diría que sí, aunque pensándolo bien haría que te esforzaras más jaja, el punto es que obviamente no tendrás todo de mí porque para ello, el intercambio debería ser equivalente.</p>
+
+                            <p className="my-5 font-medium text-2xl">Ya la vida nos dirá qué depara el camino, lo que sí es que yo por mi parte lo que más odio es arrepentirme de no hacer algo, y desde hace un tiempo me dije que no quería dejarte pasar sin más, así que dentro de mi egoísmo también intentaré lo que pueda en ese aspecto, al final solo quería decírtelo de alguna manera.</p>
+
+                            <p className="my-5 font-medium text-2xl">Bueno, y por último y más importante, más allá de pensamientos de qué es lo mejor o no para cada uno, mi interés por tu bienestar es genuino y aunque pasen las cosas que pasen, mientras siga yo por acá, en la medida de lo posible me gustaría ayudarte (no soy Superman jajaja ni mucho menos un salvador), pero al menos quisiera transmitirte que la soledad no es absoluta.</p>
+
+                            <p className="my-5 font-medium text-2xl">En fin, creo que eso sería todo de momento, corazón de melocotón.</p>
+
+                            <p className="my-5 font-medium text-2xl">Realmente te quiero mucho.</p>
+
+                            <p className="my-5 font-medium text-2xl">Si de momento no puedes creer en ti, cree en mí que cree en ti.</p>
+
+                            <p className="my-5 font-medium text-2xl">Eso es todo de momento, cabeza de ahuyama.</p>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
---
+++ src/app/components/Music.jsx
@@ -0,0 +1,30 @@
+'use client'
+import { useState } from 'react';
+import { FaPlayCircle, FaPauseCircle } from "react-icons/fa";
+
+export default function Music({ track1 }) {
+    const [isPlaying1, setIsPlaying1] = useState(false);
+
+    const handlePlayPause1 = () => {
+        const audio = document.getElementById('track1');
+        if (isPlaying1) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+        setIsPlaying1(!isPlaying1);
+    };
+
+    return (
+        <div className="music-page">
+            <div>
+                <button onClick={handlePlayPause1}>
+                    {isPlaying1
+                        ? <FaPauseCircle size={50} fill='red' />
+                        : <FaPlayCircle size={50} fill='red' />} Track 1
+                </button>
+                <audio id="track1" src={track1}></audio>
+            </div>
+        </div>
+    );
+}
